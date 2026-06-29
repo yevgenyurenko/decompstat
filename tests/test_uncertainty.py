b7cc2ff747d1948d40cad20523c98feecd422e6c
Yevgen Yurenko <yevgen.yurenko@gmail.com>
2026-06-29 20:31:24 +0200

    Add iid uncertainty sanity test

--- tests/test_uncertainty.py
+++ tests/test_uncertainty.py
@@ -24,3 +24,13 @@ def test_block_bootstrap_ci_wider_than_naive_sem_scale_for_ar1():
     width = (res.ci_high - res.ci_low) / 2.0
     assert res.n_eff < res.n
     assert width > naive_sem(x)
+
+def test_iid_statistical_inefficiency_close_to_one():
+    rng = np.random.default_rng(123)
+    x = rng.normal(size=1000)
+    g = statistical_inefficiency(x)
+    res = block_bootstrap_mean(x, n_boot=200, seed=4)
+
+    assert 1.0 <= g < 1.5
+    assert res.n_eff > 0.65 * res.n
+    assert res.block_length <= 2
